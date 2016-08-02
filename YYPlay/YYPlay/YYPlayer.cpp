8694a1f5b44a3ba675310b2e2721071fc9609e48
Harman-Jeremywang <jeremy.wang@harman.com>
2016-08-02 22:40:29 +0800

    add ffmpeg

---
+++ YYPlay/YYPlay/YYPlayer.cpp
@@ -0,0 +1,14 @@
+//
+//  YYPlayer.cpp
+//  YYPlay
+//
+//  Created by jeremy on 8/2/16.
+//  Copyright © 2016 MF. All rights reserved.
+//
+
+#include "YYPlayer.hpp"
+
+YYPlayer::YYPlayer()
+{
+//    av_register_all();
+}
---
+++ YYPlay/YYPlay/YYPlayer.hpp
@@ -0,0 +1,22 @@
+//
+//  YYPlayer.hpp
+//  YYPlay
+//
+//  Created by jeremy on 8/2/16.
+//  Copyright © 2016 MF. All rights reserved.
+//
+
+#ifndef YYPlayer_hpp
+#define YYPlayer_hpp
+
+#include <stdio.h>
+#include "avformat.h"
+
+class YYPlayer {
+    YYPlayer();
+    
+public:
+    int time;
+};
+
+#endif /* YYPlayer_hpp */
