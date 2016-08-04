8a99604942526f6584fbf2b781814c4367fe6af8
Harman-Jeremywang <jeremy.wang@harman.com>
2016-08-04 16:56:58 +0800

    update ffmpeg demo

--- YYPlay/YYPlay/FFMpegViewController.m
+++ YYPlay/YYPlay/FFMpegViewController.m
@@ -69,13 +69,15 @@
     }];
     
     _textView = [[UITextView alloc] init];
-    [_textView setTextColor:[UIColor jc_tomatoColor]];
+    [_textView setTextColor:[UIColor jc_coolGreyColor]];
     [_textView setText:@"Wait for button action"];
+    [_textView setBackgroundColor:[UIColor jc_whiteColor]];
     [self.view addSubview:_textView];
     [_textView mas_makeConstraints:^(MASConstraintMaker *make) {
         make.left.equalTo(self.view.mas_left);
         make.right.equalTo(self.view.mas_right);
         make.bottom.equalTo(self.view.mas_bottom).with.offset(-MiniPlayerViewHeight);
+        make.top.equalTo(_configBTN.mas_bottom).with.offset(10);
     }];
 
 }
@@ -83,6 +85,7 @@
 - (void)viewWillAppear:(BOOL)animated
 {
     [super viewWillAppear:animated];
+    av_register_all();
 }
 
 - (void)didReceiveMemoryWarning {
@@ -105,16 +108,116 @@
 {
     if (sender == _configBTN) {
         NSLog(@"----------------------config button pressed----------------------\n");
+        [self printFFmpegConfig];
         
     } else if (sender == _protocolBTN) {
         NSLog(@"----------------------protocol button pressed----------------------\n");
+        [self printFFMpegProtocol];
     } else if (sender == _avformatBTN) {
         NSLog(@"----------------------avformat button pressed----------------------\n");
-    
+        [self printFFMpegAvformat];
     } else if (sender == _avfilterBTN) {
         NSLog(@"----------------------avfilter button pressed----------------------\n");
+        [self printFFMpegAvfilter];
     } else if (sender == _avcodecBTN) {
         NSLog(@"----------------------avcodec button pressed----------------------\n");
+        [self printFFMpegAvcodec];
     }
+}
+
+- (void)printFFmpegConfig
+{
+    
+    NSString *configStr = [NSString stringWithFormat:@"FFMpeg config = %s", avformat_configuration()];
+    
+    [_textView setText:configStr];
+}
+
+- (void)printFFMpegProtocol
+{
+    NSMutableString *mutableStr = [NSMutableString new];
+    char info[40000] = {0};
+    struct URLProtocol *pup = NULL;
+    //input
+    struct URLProtocol **p_temp = &pup;
+    NSString *returnProtocol = [NSString stringWithFormat:@"%10s", avio_enum_protocols((void **)p_temp, 0)];
+    NSLog(@"print return input Protocol = %@", returnProtocol);
+    while ((*p_temp) != NULL) {
+         //sprintf(info, "%s[In ][%10s]\n", info, avio_enum_protocols((void **)p_temp, 0));
+        [mutableStr appendFormat:@"[In ][%10s]\n", avio_enum_protocols((void **)p_temp, 0)];
+    }
+    
+    //output
+    
+    avio_enum_protocols((void **)p_temp, 1);
+    while ((*p_temp) != NULL) {
+        [mutableStr appendFormat:@"[Out][%10s]\n", avio_enum_protocols((void **)p_temp, 1)];
+    }
+    
+    [_textView setText:mutableStr];
+}
+
+- (void)printFFMpegAvformat
+{
+    AVInputFormat *if_temp = av_iformat_next(NULL);
+    AVOutputFormat *of_temp = av_oformat_next(NULL);
+    
+    NSMutableString *avFormatStr = [NSMutableString new];
+    //input
+    while (if_temp != NULL) {
+        [avFormatStr appendFormat:@"[In ]%10s\n", if_temp->name];
+        if_temp = if_temp->next;
+    }
+    
+    //out put
+    while (of_temp) {
+        [avFormatStr appendFormat:@"[Out]%10s\n", of_temp->name];
+        of_temp = of_temp->next;
+    }
+    
+    [_textView setText:avFormatStr];
+}
+
+- (void)printFFMpegAvfilter
+{
+    AVFilter *f_temp = (AVFilter *)avfilter_next(NULL);
+    NSMutableString *avfilterStr = [NSMutableString new];
+    while (f_temp != NULL) {
+        [avfilterStr appendFormat:@"[%10s]\n", f_temp->name];
+        f_temp = f_temp->next;
+    }
+    
+    [_textView setText:avfilterStr];
+}
+
+- (void)printFFMpegAvcodec
+{
+    AVCodec *c_temp = av_codec_next(NULL);
+    NSMutableString *avcodecStr = [NSMutableString new];
+    
+    while (c_temp != NULL) {
+        if (c_temp->decode != NULL) {
+            [avcodecStr appendFormat:@"[Dec]"];
+        }
+        if (c_temp->encode2 != NULL){
+            [avcodecStr appendFormat:@"[Enc]"];
+        }
+        switch (c_temp->type) {
+            case AVMEDIA_TYPE_VIDEO:
+                [avcodecStr appendFormat:@"[Video]"];
+                break;
+            case AVMEDIA_TYPE_AUDIO:
+                [avcodecStr appendFormat:@"[Audio]"];
+                break;
+            default:
+                [avcodecStr appendFormat:@"[Other]"];
+                break;
+        }
+        [avcodecStr appendFormat:@"%10s\n", c_temp->name];
+        
+        c_temp = c_temp->next;
+    }
+    
+    [_textView setText:avcodecStr];
 }
 @end
